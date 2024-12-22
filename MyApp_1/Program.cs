565f36fb71eb9ff472343b974c5e7281f8fcd2d3
Jack Mull <jackcmull@gmail.com>
2024-12-21 22:20:32 -0500

    Added HomeController and Home Index view. Started setting up default layout.

--- MyApp_1/Program.cs
+++ MyApp_1/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=home}/{action=index}/{id?}"
+);
+
 
 app.Run();
